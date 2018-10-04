e72281dd1fc92ab4ba7fe9bef6e9e1c16b8dd3e6
Rohit Kumar <rohit9075@gmail.com>
2018-10-04 15:41:34 +0530

    Initial commit

---
+++ app/src/main/java/com/rohit/fragmentaddtobackstack/FragmentA.java
@@ -0,0 +1,20 @@
+package com.rohit.fragmentaddtobackstack;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentA extends Fragment {
+
+	private static final String TAG = FragmentA.class.getSimpleName();
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+		View view = inflater.inflate(R.layout.fragment_a, container, false);
+
+		return view;
+	}
+}
